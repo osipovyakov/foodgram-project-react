928171df059f3dfea357f1bbf4c4ba781c16bfaa
Яков Осипов <osipovyakovv@gmail.com>
2023-06-06 21:50:58 +0700

    Фикс багов

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.46.120', '127.0.0.1', 'localhost']
 
 
 # Application definition
