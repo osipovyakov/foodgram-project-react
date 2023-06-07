763ff29d396b81f4b6a5f48df05dc36b3f5e5800
Яков Осипов <osipovyakovv@gmail.com>
2023-06-07 22:15:32 +0700

    Фильтрация по Тегам

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.46.120', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.101.80', '127.0.0.1', 'localhost']
 
 
 # Application definition
