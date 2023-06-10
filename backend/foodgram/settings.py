252ed17e4029bbca523b28118bafe1234d193d1a
Яков Осипов <osipovyakovv@gmail.com>
2023-06-11 00:07:21 +0700

    Ревью v2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.101.80', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.51.74', '127.0.0.1', 'localhost']
 
 
 # Application definition
