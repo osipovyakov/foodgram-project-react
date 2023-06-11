f3b014d02bea556ca2be8b60163ed13c9041ecce
Яков Осипов <osipovyakovv@gmail.com>
2023-06-11 21:19:04 +0700

    Ревью v2

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,8 +2,7 @@ from django.urls import include, path
 from djoser.views import TokenDestroyView
 from rest_framework.routers import DefaultRouter
 
-from .views import (CustomTokenCreateView, CustomUserView, FollowViewSet,
-                    ListSubscriberViewSet)
+from .views import (CustomTokenCreateView, CustomUserView,)
 
 router = DefaultRouter()
 
@@ -13,8 +12,8 @@ urlpatterns = [
 
     path('me/', CustomUserView.as_view({'get': 'me'})),
     path('<int:pk>/', CustomUserView.as_view({'get': 'retrieve'})),
-    path('subscriptions/', ListSubscriberViewSet.as_view({'get': 'list'})),
-    path('<int:pk>/subscribe/', FollowViewSet.as_view()),
+    path('subscriptions/', CustomUserView.as_view({'get': 'list'})),
+    path('<int:pk>/subscribe/', CustomUserView.as_view()),
     path('token/login/', CustomTokenCreateView.as_view()),
     path('token/logout/', TokenDestroyView.as_view()),
     path('', include(router.urls)),
