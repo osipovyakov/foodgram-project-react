507d887f8a7ba1a2d4519d5a67f67503ec597129
Яков Осипов <osipovyakovv@gmail.com>
2023-06-11 23:19:19 +0700

    Ревью v2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -145,12 +145,14 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
