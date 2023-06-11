4d049bf37c41bc022b7d7cc8982df1c11f90b299
Яков Осипов <osipovyakovv@gmail.com>
2023-06-11 18:01:59 +0700

    Ревью v2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -36,11 +36,9 @@ class CustomUserSerializer(UserSerializer):
         fields = UserSerializer.Meta.fields + ('is_subscribed',)
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_anonymous:
-            return False
         return Follow.objects.filter(
-            user=self.context['request'].user.id, follower=obj.id).exists()
+            user=self.context['request'].user.id, follower=obj.id
+        ).exists()
 
 
 class TagSerializer(serializers.ModelSerializer):
