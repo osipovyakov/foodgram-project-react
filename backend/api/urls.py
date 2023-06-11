ed59da89aa92c189a229cbb039a4fda2e1e16fc0
Яков Осипов <osipovyakovv@gmail.com>
2023-06-11 22:52:35 +0700

    Ревью v2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users.views import CustomUserView
-
 from api.views import IngredientsViewSet, RecipeViewSet, TagViewSet
 
 router = DefaultRouter()
