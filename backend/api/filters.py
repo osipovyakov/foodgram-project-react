f9f5b8922b0ff2f0d5157fadf0f94aa0689d7e01
Яков Осипов <osipovyakovv@gmail.com>
2023-06-12 03:55:25 +0700

    Ревью v2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -24,11 +24,13 @@ class RecipeFilterSet(django_filters.FilterSet):
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(favorite__user=self.request.user)
+        user = self.requset.user
+        if value and not user.is_anonymous:
+            return queryset.filter(favorites__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(shopping_cart__user=self.request.user)
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shopping_cart__user=user)
         return queryset
