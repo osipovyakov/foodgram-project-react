a32133566445d0b45ceb048aa7b2eb520330f277
Яков Осипов <osipovyakovv@gmail.com>
2023-06-16 03:06:36 +0700

    Пуш на сервер

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -27,7 +27,7 @@ class RecipeFilterSet(django_filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags',)
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart',)
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.requset.user
