b7b83629be3223feec96bc41e8dc0926702332cf
Яков Осипов <osipovyakovv@gmail.com>
2023-06-17 18:44:19 +0700

    Исправление ошибок на сайте

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -30,13 +30,13 @@ class RecipeFilterSet(django_filters.FilterSet):
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart',)
 
     def filter_is_favorited(self, queryset, name, value):
-        user = self.requset.user
+        author = self.requset.user
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorite__user=user)
+            return queryset.filter(favorite__author=author)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
+        author = self.request.user
         if value and self.request.user.is_authenticated:
-            return queryset.filter(shopping_cart__user=user)
+            return queryset.filter(shopping_cart__author=author)
         return queryset
