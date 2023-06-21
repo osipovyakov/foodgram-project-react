907f5242c84b20c3c33c476afbd60180ca0a2450
Яков Осипов <osipovyakovv@gmail.com>
2023-06-21 12:05:20 +0700

    Исправление Избранного

--- backend/api/views.py
+++ backend/api/views.py
@@ -104,7 +104,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         ingredients = RecipeIngredient.objects.filter(
-            recipe__shopping_cart__ingredient_recipeingredient__user=user
+            recipe__shopping_cart__user=request.user
         ).values(
             'ingredient__name',
             'ingredient__measurement_unit'
